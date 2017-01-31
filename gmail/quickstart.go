5ec551b4a242deffb81a305fe3c11a4713d9a126
remotejob <support@mazurov.eu>
2017-01-31 09:36:47 +0200

    dev

--- gmail/quickstart.go
+++ gmail/quickstart.go
@@ -1,179 +1,194 @@
-
-
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "net/http"
-  "net/url"
-  "os"
-  "os/user"
-  "path/filepath"
-  
-  "golang.org/x/net/context"
-  "golang.org/x/oauth2"
-  "golang.org/x/oauth2/google"
-  "google.golang.org/api/gmail/v1"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	mgmail "google.golang.org/api/gmail/v1"
 )
 
 // getClient uses a Context and Config to retrieve a Token
 // then generate a Client. It returns the generated Client.
 func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-  cacheFile, err := tokenCacheFile()
-  if err != nil {
-    log.Fatalf("Unable to get path to cached credential file. %v", err)
-  }
-  tok, err := tokenFromFile(cacheFile)
-  if err != nil {
-    tok = getTokenFromWeb(config)
-    saveToken(cacheFile, tok)
-  }
-  return config.Client(ctx, tok)
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
 }
 
 // getTokenFromWeb uses Config to request a Token.
 // It returns the retrieved Token.
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-  authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-  fmt.Printf("Go to the following link in your browser then type the "+
-    "authorization code: \n%v\n", authURL)
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
 
-  var code string
-  if _, err := fmt.Scan(&code); err != nil {
-    log.Fatalf("Unable to read authorization code %v", err)
-  }
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
 
-  tok, err := config.Exchange(oauth2.NoContext, code)
-  if err != nil {
-    log.Fatalf("Unable to retrieve token from web %v", err)
-  }
-  return tok
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
 }
 
 // tokenCacheFile generates credential file path/filename.
 // It returns the generated credential path/filename.
 func tokenCacheFile() (string, error) {
-  usr, err := user.Current()
-  if err != nil {
-    return "", err
-  }
-  tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
-  os.MkdirAll(tokenCacheDir, 0700)
-  return filepath.Join(tokenCacheDir,
-    url.QueryEscape("gmail-go-quickstart.json")), err
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("gmail-go-quickstart.json")), err
 }
 
 // tokenFromFile retrieves a Token from a given file path.
 // It returns the retrieved Token and any read error encountered.
 func tokenFromFile(file string) (*oauth2.Token, error) {
-  f, err := os.Open(file)
-  if err != nil {
-    return nil, err
-  }
-  t := &oauth2.Token{}
-  err = json.NewDecoder(f).Decode(t)
-  defer f.Close()
-  return t, err
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
 }
 
 // saveToken uses a file path to create a file and store the
 // token in it.
 func saveToken(file string, token *oauth2.Token) {
-  fmt.Printf("Saving credential file to: %s\n", file)
-  f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-  if err != nil {
-    log.Fatalf("Unable to cache oauth token: %v", err)
-  }
-  defer f.Close()
-  json.NewEncoder(f).Encode(token)
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
 }
 
 func main() {
-  ctx := context.Background()
+	ctx := context.Background()
 
-  b, err := ioutil.ReadFile("client_secret.json")
-  if err != nil {
-    log.Fatalf("Unable to read client secret file: %v", err)
-  }
+	b, err := ioutil.ReadFile("client_secret.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
 
-  // If modifying these scopes, delete your previously saved credentials
-  // at ~/.credentials/gmail-go-quickstart.json
-  config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
-  if err != nil {
-    log.Fatalf("Unable to parse client secret file to config: %v", err)
-  }
-  client := getClient(ctx, config)
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/gmail-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, mgmail.GmailReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
 
-  srv, err := gmail.New(client)
-  if err != nil {
-    log.Fatalf("Unable to retrieve gmail Client %v", err)
-  }
+	srv, err := mgmail.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve gmail Client %v", err)
+	}
 
-  user := "me"
-//   r, err := srv.Users.Labels.List(user).Do()
-//   if err != nil {
-//     log.Fatalf("Unable to retrieve labels. %v", err)
-//   }
-//   if (len(r.Labels) > 0) {
-//     fmt.Print("Labels:\n")
-//     for _, l := range r.Labels {
-//       fmt.Printf("- %s\n",  l.Name)
-//     }
-//   } else {
-//     fmt.Print("No labels found.")
-//   }
+	user := "me"
+	//   r, err := srv.Users.Labels.List(user).Do()
+	//   if err != nil {
+	//     log.Fatalf("Unable to retrieve labels. %v", err)
+	//   }
+	//   if (len(r.Labels) > 0) {
+	//     fmt.Print("Labels:\n")
+	//     for _, l := range r.Labels {
+	//       fmt.Printf("- %s\n",  l.Name)
+	//     }
+	//   } else {
+	//     fmt.Print("No labels found.")
+	//   } log.Println(part.MimeType,part.Body.Data)
 
-  job, err := srv.Users.Messages.List(user).Q("to:programming@alaston.fi and newer_than:10d").Do()
-  if err != nil {
-    log.Fatalf("Unable to retrieve labels. %v", err)
-  }
- for _, m := range job.Messages {
+	job, err := srv.Users.Messages.List(user).Q("to:alexander@rouvat.fi and newer_than:10d").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve labels. %v", err)
+	}
+	for _, m := range job.Messages {
 
-   
-     	fmt.Printf("\nMessage URL: https://mail.google.com/mail/u/0/#all/%v\n", m.Id)
+		// fmt.Printf("\nMessage URL: https://mail.google.com/mail/u/0/#all/%v\n", m.Id)
+
+		//  log.Println(m.Raw)
+		// Create a new gmail service using the client
 		// fmt.Printf("Size: %v, Date: %v, Snippet: %q\n", m.size, m.date, m.snippet)
 
+		msg, err := srv.Users.Messages.Get(user, m.Id).Format("full").Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve message %v: %v", m.Id, err)
+		}
 
+		// log.Println(msg.Payload.Parts)
 
-        msg, err := srv.Users.Messages.Get(user, m.Id).Do()
-			if err != nil {
-				log.Fatalf("Unable to retrieve message %v: %v", m.Id, err)
+		for _, part := range msg.Payload.Parts {
+
+			// log.Println(msg.Raw)
+			// for _, part := range msg. {
+			// 	log.Println(string(part))
+
+			// data, err := base64.StdEncoding.DecodeString(msg.Raw)
+			// if err != nil {
+			// 	log.Fatalf(err.Error())
+			// }
+
+			// html := string(data)
+			// fmt.Println("html" + html)
+			// }
+			log.Println(part.MimeType)
+
+			if part.MimeType == "multipart/alternative" {
+
+				// log.Println(part.Parts[0])
+
+				// log.Println(part.Parts[1].Body.MarshalJSON())
+
+				// for _, mpart := range part.Parts {
+
+				// 	log.Println(mpart)
+				data, _ := base64.URLEncoding.DecodeString(part.Parts[1].Body.Data)
+				// data, _ := base64.URLEncoding.DecodeString(part.Parts[0].Body.Data)
+				html := string(data)
+				fmt.Println(html)
+
 			}
 
-        // log.Println(msg)
+		}
 
-        for _, h := range msg.Payload.Headers {
-				if h.Name == "Date" {
-					
-                    log.Println(h.Value)
+		//   for _, h := range msg.Payload.Headers {
 
+		//     log.Println(h)
+		// 	// if h.Name == "Date" {
 
-					break
-				}
-			}
+		//   //             log.Println(h.Value)
 
+		// 	// 	break
+		// 	// }
+		// }
 
-
-
-
-
-
-
-
-    //  msg, err := svc.Users.Messages.Get("me", m.Id).Do()
-	// 		if err != nil {
-	// 			log.Fatalf("Unable to retrieve message %v: %v", m.Id, err)
-	// 		}
-
-
-    //         log.Println(msg)
-
-
- }   
-
-
+	}
 
 }
